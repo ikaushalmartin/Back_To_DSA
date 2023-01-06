b2a229660c2b4f8a3bb85311119d9aefad10a547
ikaushalmartin <kaushalmartin@gmail.com>
2023-01-06 17:35:40 +0530

    quick_sort

--- recursion/quicksort.cpp
+++ recursion/quicksort.cpp
@@ -3,6 +3,37 @@ using namespace std;
 
 int pivot(int arr[], int start, int end){
 
+    int pivot=arr[start];
+    int counter=0;
+    for(int i=start+1;i<=end;i++){
+        if(arr[i]<=pivot){
+            counter++;
+        }
+    }
+
+    swap(arr[start+counter],arr[start]);
+
+    int i=start,j=end;
+
+    while(i<start+counter &&j>start+counter){
+
+        while(arr[i]<pivot){ 
+            i++;
+        }
+
+        while(arr[j]>pivot){
+            j--;
+        }
+
+        if(i>start+counter && j<start+counter){
+            swap(arr[i],arr[j]);
+            i++;j--;
+        }
+    }
+
+return start+counter;
+
+
 }
 
 
@@ -22,8 +53,12 @@ void quicksort(int arr[],  int start, int end){
 }
 
 int main(){
-    int arr[6]={4,1,5,3,2,8};
-    int n=6;
+    int arr[4]={6,2,4,1};
+    int n=4;
     quicksort(arr,0,n-1);
 
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
 }
