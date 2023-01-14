d790a0f1d44875c3d5364e3db0617edbd535d886
ikaushalmartin <kaushalmartin@gmail.com>
2023-01-15 01:57:07 +0530

    rec

--- firstandlastoccurenceofelement.cpp
+++ firstandlastoccurenceofelement.cpp
@@ -3,32 +3,41 @@
 using namespace std;
 
 
-static int first =-1;
+//static int first =-1;
   
 
-
+/*
 void findelement(vector<int>&output, string a, char find,int start, int end){
-
-
 
     if(start>=end){
         return ;
-    }
-    
-    if(a[start]==find&&first==-1){
+    }  
+    if(a[start]==find){
         output.push_back(start);
-        
+         
     }
-    else if (a[start]==find)
-    {
-       output.push_back(end);
-       return;
-
-    }
-
     findelement(output,a,find,start+1,end);
-     
+}
+*/
+
+static int first=-1;
+static int last=-1;
+void findelement_second(string a, char b, int index){
     
+    if(index==a.size()){
+        return;
+    }
+
+    if(a[index]==b){
+        if(first==-1){
+            first=index;
+        }
+        else{
+            last=index;
+        }
+    }
+
+findelement_second(a,b,index+1);
 
 }
 
@@ -37,10 +46,10 @@ int main(){
     char find='a';
 
     vector<int>output;
+    findelement_second(a,find,0);
+   // findelement(output,a,find,0, a.size());
+   // cout<<output.front()<<endl;
+   // cout<<output[output.size()-1];
 
-    findelement(output,a,find,0, a.size());
-
-    cout<<output.front()<<endl;
-    
-    cout<<output[output.size()-1];
+   cout<<"first = "<<first<<"     "<<"last = "<<last<<endl;
 }
