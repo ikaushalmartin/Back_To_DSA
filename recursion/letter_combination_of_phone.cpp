640d3f72d7eb496d57a15742786d7b0e61707e41
ikaushalmartin <kaushalmartin@gmail.com>
2023-01-15 16:17:17 +0530

    phone_prob

--- recursion/letter_combination_of_phone.cpp
+++ recursion/letter_combination_of_phone.cpp
@@ -1,14 +1,34 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int main(){
-    string str="abc";
-    int index=1;
-    for(int i=index+1;i!=index;i++){
-        if(i>=str.size()){
-            i=0;
+void solve(string digit,  vector<string> &ans, string output, vector<string> mapping, int index){
 
-        }
-        cout<<i<<" ";
+
+if(index==digit.size()){
+    ans.push_back(output);
+    return;
+}
+
+int digit_from_str=digit[index]-'0';
+string str_from_mapping=mapping[digit_from_str];
+
+for(int i=0;i<str_from_mapping.size();i++){
+    output.push_back(str_from_mapping[i]);
+    solve(digit,ans,output,mapping,index+1);
+    output.pop_back();
+}
+
+}
+
+int main(){
+    string digit="23";
+    vector<string> mapping={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};   
+     vector<string> ans;
+    string output;
+    solve(digit,ans,output,mapping,0);
+
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<"  ";
     }
    }
