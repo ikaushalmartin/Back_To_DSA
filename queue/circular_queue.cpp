8f462ea8c2225614393f975c3d1a8497b9932d8e
ikaushalmartin <kaushalmartin@gmail.com>
2023-05-30 17:23:41 +0530

    queue

--- queue/circular_queue.cpp
+++ queue/circular_queue.cpp
@@ -1,59 +1,97 @@
-#include<iostream>
-using namespace std;
-
-class CircularQueue{
-    public:
-    int start;
-    int back;
+class MyCircularQueue {
+    int front;
+    int rear;
     int size;
     int *arr;
-    CircularQueue(int n){
-        start=-1;
-        back=-1;
-        size=n;
+public:
+    MyCircularQueue(int k) {
+        front=-1;
+        rear=-1;
+        size=k;
         arr=new int[size];
     }
-
-    // Enqueues 'X' into the queue. Returns true if it gets pushed into the stack, and false otherwise.
-    bool enqueue(int value){
-      if ((start == 0&&back == size - 1 ) || (back == (start-1)%(size - 1))) {
+    
+    bool enQueue(int value) {
+        if ((front == 0&&rear == size - 1 ) || (rear == front-1 )) {
       return false;
-      }else if(start==-1){
-          back=0;
-          start=0;
+      }else if(front==-1){
+          front=rear=0;
+        
           
-      }else if(back==size-1 && start!=0){
-          back=0;
+      }else if(rear==size-1 && front!=0){
+          rear=0;
           
       }else{
-          back++;
+          rear++;
       }
 
-      arr[back]=value;
+      arr[rear]=value;
       return true;
-
-
     }
+    
+    bool deQueue() {
+        if(front==-1){
+            return false;
+        }else if(front==rear){
+            front=rear=-1;
+        }
+        else if(front==size-1){
+            front =0;
+        } else{
+            front++;
+        }
 
-    // Dequeues top element from queue. Returns -1 if the stack is empty, otherwise returns the popped element.
-    int dequeue(){
-        if(start == -1 ){
+        return true;
+        
+    }
+    
+    int Front() {
+
+        if(front==-1){
             return -1;
-        }
-        int ans=arr[start];
- arr[start]=-1;
-        if (start == back) {
-        start=back=-1;
-        }
-        else if(start ==size-1){
-            start =0;
         }else{
-            start++;
+            return arr[front];
         }
-        return ans;
+        
+    }
+    
+    int Rear() {
+
+        if(front==-1){
+            return -1;
+        }else{
+            return arr[rear];
+        }
+        
+    }
+    
+    bool isEmpty() {
+        if(front==-1){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+    
+    bool isFull() {
+
+       if((rear==size-1 && front==0) || (front !=0 &&rear==(front-1)%(size-1))){
+            return true;
+        }else{
+            return false;
+        }
+        
     }
 };
 
-int main(){
-
-}
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
